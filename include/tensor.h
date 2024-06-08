02075e299d40a6251d2e09f740d6c333dc941e8b
Yejoon Lee <leeyejoon@snu.ac.kr>
2024-06-08 16:16:40 +0900

    [MAJOR] (Verified) GPU alloc support for Tensor

--- include/tensor.h
+++ include/tensor.h
@@ -9,9 +9,10 @@ struct Tensor {
   size_t ndim = 0;
   size_t shape[4];
   float *buf = nullptr;
+  int device = -1; // -1 for CPU, 0~3 for GPU
 
-  Tensor(const vector<size_t> &shape_);
-  Tensor(const vector<size_t> &shape_, float *buf_);
+  Tensor(const vector<size_t> &shape, int device=-1);
+  Tensor(const vector<size_t> &shape_, float *buf_, int device=-1); 
   ~Tensor();
 
   size_t num_elem();
--- src/tensor.cu
+++ src/tensor.cu
@@ -8,23 +8,40 @@
  * @member - shape: Shape of tensor from outermost dimension to innermost
  dimension e.g., {{1.0, -0.5, 2.3}, {4.3, 5.6, -7.8}} => shape = {2, 3}
  */
-Tensor::Tensor(const vector<size_t> &shape_) {
+Tensor::Tensor(const vector<size_t> &shape_, int device) : device(device) {
   ndim = shape_.size();
   for (size_t i = 0; i < ndim; i++) { shape[i] = shape_[i]; }
   size_t N_ = num_elem();
-  buf = (float *) calloc(N_, sizeof(float));
+
+  if (device >= 0) {
+    cudaSetDevice(device);
+    cudaMalloc(&buf, N_ * sizeof(float));
+    cudaMemset(buf, 0, N_ * sizeof(float));
+  } else {
+    buf = (float *) calloc(N_, sizeof(float));
+  }
 }
 
-Tensor::Tensor(const vector<size_t> &shape_, float *buf_) {
+Tensor::Tensor(const vector<size_t> &shape_, float *buf_, int device) : device(device) {
   ndim = shape_.size();
   for (size_t i = 0; i < ndim; i++) { shape[i] = shape_[i]; }
   size_t N_ = num_elem();
-  buf = (float *) malloc(N_ * sizeof(float));
-  memcpy(buf, buf_, N_ * sizeof(float));
+
+  if (device >= 0) {
+    cudaSetDevice(device);
+    cudaMalloc(&buf, N_ * sizeof(float));
+    cudaMemcpy(buf, buf_, N_ * sizeof(float), cudaMemcpyHostToDevice);
+  } else {  
+    buf = (float *) malloc(N_ * sizeof(float));
+    memcpy(buf, buf_, N_ * sizeof(float));
+  }
 }
 
 Tensor::~Tensor() {
-  if (buf != nullptr) free(buf);
+  if (buf != nullptr) {
+    if (device >= 0) { cudaFree(buf); }
+    else { free(buf); }
+  }
 }
 
 size_t Tensor::num_elem() {
