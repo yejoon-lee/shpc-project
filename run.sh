760d562c79b49281a0d4e1bed68622daf7ab4575
Yejoon Lee <leeyejoon@snu.ac.kr>
2024-06-16 02:20:37 +0900

    Untrack run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-: ${NODES:=1}
-
-salloc -N $NODES --partition class1 --exclusive --gres=gpu:1   \
-	mpirun --bind-to none -mca btl ^openib -npernode 1 \
-		--oversubscribe -quiet \
-		./main $@
